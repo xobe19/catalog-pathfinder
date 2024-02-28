f0c28f0150a4a19c4ed4fe9f94d592421eede9af
Hitesh Lalwani <lwni@protonmail.com>
2024-02-28 15:59:23 +0530

    working!

--- find_path.ts
+++ find_path.ts
@@ -8,73 +8,140 @@ const reserves = reserves_imp as {
     reserve1: string;
   };
 };
+
+
+let graph: {
+  [key in string]: [string, string][]
+} = {};
+
+for(let pairAddr in reserves) {
+  if(!graph[reserves[pairAddr].token0]) graph[reserves[pairAddr].token0] = [];
+  graph[reserves[pairAddr].token0].push([reserves[pairAddr].token1, pairAddr]);
+  
+  if(!graph[reserves[pairAddr].token1]) graph[reserves[pairAddr].token1] = [];
+  graph[reserves[pairAddr].token1].push([reserves[pairAddr].token0, pairAddr]);
+}
+
+
+
+
+
+let maxOut: {
+  [key in string]: bigint;
+} = {};
+
+let prev: {
+  [key in string]: string;
+} = {};
+
+function getOut(
+  pairAddr: string,
+  inTokenAddress: string,
+  outTokenAddress: string,
+  inTokenAmt: bigint
+) {
+  let in_token_res = BigInt(reserves[pairAddr].reserve1);
+  let out_token_res = BigInt(reserves[pairAddr].reserve0);
+  if (reserves[pairAddr].token1 != inTokenAddress) {
+    let tmp = in_token_res;
+    in_token_res = out_token_res;
+    out_token_res = tmp;
+  }
+
+  let qty_token1_recieve = null;
+  let st = BigInt(1),
+    en = out_token_res;
+  while (st <= en) {
+    let mid = (st + en) / BigInt(2);
+    let condition =
+      inTokenAmt < in_token_res &&
+      ((inTokenAmt + in_token_res) * BigInt(1000) -
+        inTokenAmt * BigInt(3)) *
+        ((out_token_res - mid) * BigInt(1000)) >=
+        in_token_res * out_token_res * BigInt(1000000);
+    if (condition) {
+      qty_token1_recieve = mid;
+
+      st = mid + BigInt(1);
+    } else {
+      en = mid - BigInt(1);
+    }
+  }
+  return qty_token1_recieve;
+}
+
+let inPath: {
+  [key in string]: Set<string>;
+} = {};
+
+type QueueElement = {addr: string, path: Set<string>, qty: bigint};
 function findPath(
   inTokenAddress: string,
   outTokenAddress: string,
   inAmt: bigint
 ) {
-  let maxOut: {
-    [key in string]: bigint;
+
+  let q: {
+    [key in string]: {
+      path: Set<String>,
+      qty: bigint;
+    }
   } = {};
+ 
+  q[inTokenAddress] = {
+    path: new Set(),
+    qty: inAmt
+  }
+  q[inTokenAddress].path.add(inTokenAddress);
+ 
+  let HOPS = 5;
 
-  let prev: {
-    [key in string]: string;
-  } = {};
+  while(HOPS-->0) {
+    let nq: {
+    [key in string]: {
+      path: Set<String>,
+      qty: bigint;
+    }
+  }= {};
 
-  maxOut[inTokenAddress] = inAmt;
-  prev[inTokenAddress] = "";
-  let bellman_ford = 1000;
-  while (bellman_ford-- > 0) {
-    for (let pairAddr in reserves) {
-      //token 0 to token 1
-
-      const token1_addr = reserves[pairAddr].token1;
-      const token0_addr = reserves[pairAddr].token0;
-      const res_token1 = BigInt(reserves[pairAddr].reserve1);
-      const res_token0 = BigInt(reserves[pairAddr].reserve0);
-
-      if (maxOut[token0_addr] !== undefined) {
-        let qty_token0 = maxOut[reserves[pairAddr].token0];
-        let prod = res_token0 * res_token1;
-        let qty_token1 = res_token1 - prod / (res_token0 + qty_token0);
-        if (
-          maxOut[token1_addr] === undefined ||
-          maxOut[token1_addr] < qty_token1
-        ) {
-          maxOut[token1_addr] = qty_token1;
-          prev[token1_addr] = token0_addr;
-        }
-      }
-
-      //token 1 to token 0
-
-      if (maxOut[token1_addr] !== undefined) {
-        let qty_token1 = maxOut[token1_addr];
-        let prod = res_token0 * res_token1;
-        let qty_token0 = res_token0 - prod / (res_token1 + qty_token1);
-        if (
-          (maxOut[token0_addr] === undefined ||
-            maxOut[token0_addr] < qty_token0) &&
-          prev[token1_addr] != token0_addr 
-        ) {
-          maxOut[token0_addr] = qty_token0;
-          prev[token0_addr] = token1_addr;
+  for(let addr in q) {
+    nq[addr] = {
+      path: new Set(q[addr].path),
+      qty: q[addr].qty
+    }
+  }
+    for(let addr in q) {
+      let qd = q[addr];
+      for(let [neighbour, pa] of graph[addr]) {
+         
+        if(qd.path.has(neighbour)) continue;
+        let new_qty = getOut(pa, addr, neighbour, qd.qty);
+        if(!new_qty) continue;
+        if(nq[neighbour] === undefined || nq[neighbour].qty < new_qty) {
+          let new_path = new Set(qd.path);
+          new_path.add(neighbour);
+          nq[neighbour] = {path: new_path, qty: new_qty}
         }
       }
     }
-    return prev;
+    q = nq;
   }
+
+  return q[outTokenAddress].path;
+
 }
 
 // vlink and usdc
 
-let prev = findPath(
+console.log(findPath(
   "0xdAC17F958D2ee523a2206206994597C13D831ec7",
   "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
   BigInt(10000)
-);
-let curr = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599";
-while (curr != "") {
-  console.log(curr);
-  curr = prev![curr];
-}
+));
+// console.log(inPath["0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599"]);
+// let curr = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599";
+// while (curr != "") {
+//   console.log(curr);
+//   let prv = prev[curr];
+//   curr = prv;
+// }
