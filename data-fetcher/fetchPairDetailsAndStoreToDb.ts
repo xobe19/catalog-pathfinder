dcfedee1e206374d12fe1a06dbf4093dadfb316c
Shubh A Chudasama <shubh.cs@proton.me>
2024-03-04 00:35:45 +0530

    Implement fetching pair details, Delete redundant files

--- data-fetcher/fetchPairDetailsAndStoreToDb.ts
+++ data-fetcher/fetchPairDetailsAndStoreToDb.ts
@@ -1,12 +1,19 @@
 import { Pair, PrismaClient } from "@prisma/client";
+import { Result } from "ethers";
 import fs from "fs";
 import path from "path";
 import { executeCalls, prepareCall } from ".";
 
 const prisma = new PrismaClient();
 
-async function getPairDetails(pairAddress: string) {
-  const calls = [
+type ResultWithMetadata = {
+  results: Result[];
+  start: number;
+  size: number;
+};
+
+function getCallsForPairDetails(pairAddress: string) {
+  return [
     prepareCall(
       pairAddress,
       "getReserves",
@@ -23,63 +30,119 @@ async function getPairDetails(pairAddress: string) {
       "function token1() external view returns (address)"
     ),
   ];
+}
 
-  const [reserves, token0, token1] = await executeCalls(calls);
+function getMulticallBatch(
+  pairAddresses: string[],
+  start: number,
+  size: number
+): Promise<ResultWithMetadata> {
+  const calls = [];
+  for (let i = 0; i < size; i++) {
+    calls.push(...getCallsForPairDetails(pairAddresses[i + start]));
+  }
+  const promiseOfResults = executeCalls(calls);
 
-  const token0Address = (token0[0] as bigint).toString();
-  const token1Address = (token1[0] as bigint).toString();
+  return new Promise((resolve, reject) => {
+    promiseOfResults
+      .then((results) =>
+        resolve({
+          results: results,
+          start,
+          size,
+        })
+      )
+      .catch((err) => reject(err));
+  });
+}
 
-  const ret: Pair = {
-    address: pairAddress,
-    token0Address,
-    token1Address,
-    token0Reserve: reserves[0].toString(),
-    token1Reserve: reserves[1].toString(),
-  };
+function getPairDetailsFromExecuteCallsResult(
+  pairAddress: string[],
+  executeCallsResults: Result[]
+): Pair[] {
+  const ret = [];
+
+  let j = 0;
+  for (let i = 0; i < executeCallsResults.length; i += 3) {
+    const reserves = executeCallsResults[i + 0];
+    const token0 = executeCallsResults[i + 1];
+    const token1 = executeCallsResults[i + 2];
+
+    const token0Address = (token0[0] as bigint).toString();
+    const token1Address = (token1[0] as bigint).toString();
+    const pair: Pair = {
+      address: pairAddress[j++],
+      token0Address,
+      token1Address,
+      token0Reserve: reserves[0].toString(),
+      token1Reserve: reserves[1].toString(),
+    };
+    ret.push(pair);
+  }
 
   return ret;
 }
 
+function toDbPairs(pairAddresses: string[], results: ResultWithMetadata[]) {
+  const pairs: Pair[] = [];
+  for (const resultBatch of results) {
+    let i = 0;
+
+    pairs.push(
+      ...getPairDetailsFromExecuteCallsResult(
+        pairAddresses.slice(
+          resultBatch.start,
+          resultBatch.start + resultBatch.size
+        ),
+        resultBatch.results
+      )
+    );
+  }
+  return pairs;
+}
+
 async function main() {
   console.log(new Date().toLocaleString());
-
-  const batchSize = 1000;
+  console.time("service time");
 
   // clear the db first
   await prisma.pair.deleteMany({});
   console.log("db cleared");
 
-  const pairAddressFilePath = path.join(__dirname, "addresses.csv");
+  const pairAddressesFilePath = path.join(__dirname, "addresses.csv");
 
   try {
-    const fileRef = fs.readFileSync(pairAddressFilePath, "utf-8");
-    const pairsAddress = fileRef.split("\n");
-    console.log(`Pair addresses read from ${pairAddressFilePath}`);
+    const fileRef = fs.readFileSync(pairAddressesFilePath, "utf-8");
+    const pairAddresses = fileRef.split("\n");
+    console.log(`Pair addresses read from ${pairAddressesFilePath}`);
 
-    function createBatch(start: number, end: number) {
-      const calls = [];
-      for (let i = start; i < end; i++) {
-        calls.push(getPairDetails(pairsAddress[i]));
-      }
+    // no. of contract read calls to batch into 1 RPC call
+    const multicallbatchSize = 500;
+    // no. of RPC calls to execute at a time
+    let promiseBatchSize = 20;
 
-      return calls;
-    }
-
-    const totalReqs = pairsAddress.length;
+    let promiseBatch: Promise<ResultWithMetadata>[] = [];
+    let resCount = 0;
     console.log("Pair data fetch start");
-    for (let i = 0; i < totalReqs; i += batchSize) {
-      const start = i;
-      const end = Math.min(i + batchSize, totalReqs);
-      const res = await Promise.all(createBatch(start, end));
-      await prisma.pair.createMany({ data: res });
-      console.log("batch pushed to db", i);
-      i++;
+    for (let i = 0; i < pairAddresses.length; i += multicallbatchSize) {
+      const size = Math.min(multicallbatchSize, pairAddresses.length - i);
+      const res = getMulticallBatch(pairAddresses, i, size);
+      promiseBatch.push(res);
+      resCount += 1;
+
+      if (size === pairAddresses.length - i || resCount === promiseBatchSize) {
+        const results = await Promise.all(promiseBatch);
+        console.log(toDbPairs(pairAddresses, results));
+        resCount = 0;
+        promiseBatch = [];
+      }
     }
     console.log("Pair data fetch end");
   } catch (error) {
     console.error(error);
   }
 
+  console.timeEnd("service time");
   console.log(new Date().toLocaleString());
 }
 
