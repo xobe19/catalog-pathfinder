e0c032d61c9d100458066a8174cb047689d46d66
Shubh A Chudasama <shubh.cs@proton.me>
2024-03-06 17:43:42 +0530

    Uniswap v3 some progress

---
+++ ethToUsdc.py
@@ -0,0 +1,67 @@
+import math
+
+q96 = 2**96
+eth = 10**18
+
+
+def price_to_tick(p):
+    return math.floor(math.log(p, 1.0001))
+
+
+def price_to_sqrtp(p):
+    return int(math.sqrt(p) * q96)
+
+
+def sqrtp_to_price(sqrtp):
+    return (sqrtp / q96) ** 2
+
+
+def tick_to_sqrtp(t):
+    return int((1.0001 ** (t / 2)) * q96)
+
+
+def liquidity0(amount, pa, pb):
+    if pa > pb:
+        pa, pb = pb, pa
+    return (amount * (pa * pb) / q96) / (pb - pa)
+
+
+def liquidity1(amount, pa, pb):
+    if pa > pb:
+        pa, pb = pb, pa
+    return amount * q96 / (pb - pa)
+
+
+def calc_amount0(liq, pa, pb):
+    if pa > pb:
+        pa, pb = pb, pa
+    return int(liq * q96 * (pb - pa) / pb / pa)
+
+
+def calc_amount1(liq, pa, pb):
+    if pa > pb:
+        pa, pb = pb, pa
+    return int(liq * (pb - pa) / q96)
+
+sqrtp_cur = 1276637904125314634064667762306642
+
+
+liq = 2094005660474653840
+
+
+# Swap ETH for USDC
+amount_in = 1001 * 10**6
+
+print(f"\nSelling {amount_in/10**6} ETH")
+
+price_next = int((liq * q96 * sqrtp_cur) // (liq * q96 + amount_in * sqrtp_cur))
+
+print("New price:", (price_next / q96) ** 2)
+print("New sqrtP:", price_next)
+print("New tick:", price_to_tick((price_next / q96) ** 2))
+
+amount_in = calc_amount0(liq, price_next, sqrtp_cur)
+amount_out = calc_amount1(liq, price_next, sqrtp_cur)
+
+print("ETH in:", amount_in / 10**6)
+print("USDC out:", amount_out / eth)
--- readme.md
+++ readme.md
@@ -73,6 +73,10 @@ findPath(
 let HOPS = 10;
 ```
 
+## Preprocessing:
+
+- Remove all pairs with shitcoins
+
 Shitcoins:
 USD: 0xd233d1f6fd11640081abb8db125f722b5dc729dc
 AMIGO: 0x761d38e5ddf6ccf6cf7c55759d5210750b5d60f3
