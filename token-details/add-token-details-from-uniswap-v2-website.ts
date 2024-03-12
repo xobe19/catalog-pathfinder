6e4db0f93d48e1de41bd22424471bcc5145ab77e
Shubh A Chudasama <shubh.cs@proton.me>
2024-03-12 17:02:18 +0530

    Add token data from uniswap v2 and v3

---
+++ token-details/add-token-details-from-uniswap-v2-website.ts
@@ -0,0 +1,42 @@
+import { Prisma } from "@prisma/client";
+import { prisma } from "./dbClient";
+
+import tokenData from "./uniswap-v2-website.json";
+
+async function main() {
+  console.time("done in");
+
+  const tokens = tokenData.data;
+
+  for (const token of tokens) {
+    const { id, symbol, name } = token;
+    try {
+      const updated = await prisma.token.update({
+        where: { id: id.toLowerCase() },
+        data: {
+          symbol,
+          name,
+        },
+      });
+      console.log(`updated ${updated.id} ${updated.name} (${updated.symbol})`);
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        /* https://www.prisma.io/docs/orm/reference/error-reference#p2025 */
+        // Record to update not found
+        if (e.code === "P2025") {
+          await prisma.token.create({
+            data: {
+              id: id.toLowerCase(),
+              symbol,
+              name,
+            },
+          });
+          console.log(`Added new token ${id} to db`);
+        }
+      }
+    }
+  }
+
+  console.timeEnd("done in");
+}
+main();
