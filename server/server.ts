b21a11e783b8c5ddf4f55a718ab91f8b329e1af4
0xvikasrushi <0xvikas@gmail.com>
2024-03-22 15:48:57 +0530

    cors bug

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,14 @@
 import dotenv from "dotenv";
 import express from "express";
 import { router } from "./src/routes";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/", router);
-app.use(cors());
 
 const PORT = parseInt(process.env.PORT!);
 if (!PORT) throw new Error("Please set the PORT environment variable.");
@@ -15,6 +16,3 @@ if (!PORT) throw new Error("Please set the PORT environment variable.");
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
-function cors(): any {
-  throw new Error("Function not implemented.");
-}
