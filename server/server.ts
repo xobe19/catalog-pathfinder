83cb78ef306f9180578e6e1d9fc7aba4dcdce9ae
0xvikasrushi <0xvikas@gmail.com>
2024-03-22 15:40:51 +0530

    add cors to server

--- server/server.ts
+++ server/server.ts
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/", router);
+app.use(cors());
 
 const PORT = parseInt(process.env.PORT!);
 if (!PORT) throw new Error("Please set the PORT environment variable.");
@@ -14,3 +15,6 @@ if (!PORT) throw new Error("Please set the PORT environment variable.");
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
+function cors(): any {
+  throw new Error("Function not implemented.");
+}
